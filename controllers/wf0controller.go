1a4bdc7f44df1f2f81f1a998a214fe842937fc1c
a good neighbor <shared@complicated.com>
2018-02-06 06:31:42 +0000

    wf0 controller, view, static/

---
+++ controllers/wf0controller.go
@@ -0,0 +1,16 @@
+package controllers
+
+import (
+	"github.com/astaxie/beego"
+)
+
+type WF0Controller struct {
+	beego.Controller
+}
+
+func (c *WF0Controller) CreatePage() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.TplName = "wf0.tpl"
+	c.Render()
+}
