fb66e717f41d4912fd2ce83565a8465a648bfdf7
a good neighbor <shared@complicated.com>
2018-02-06 23:11:36 +0000

    main, SSL

--- main.go
+++ main.go
@@ -10,6 +10,16 @@ func main() {
 	beego.SetStaticPath("/css", "static/css")
 	beego.SetStaticPath("/vendor", "static/vendor")
 	beego.SetStaticPath("/app", "static/app")
+	beego.SetStaticPath("/reactsite", "static/reactsite")
+	beego.SetStaticPath("/static", "static/reactsite/static")
+	beego.SetStaticPath("/", "static/reactsite")
+
+	beego.BConfig.Listen.EnableHTTP = false
+	beego.BConfig.Listen.EnableHTTPS = true
+	beego.BConfig.Listen.HTTPSPort  = 8080
+	beego.BConfig.Listen.HTTPSCertFile  = "conf/ssl.crt"
+	beego.BConfig.Listen.HTTPSKeyFile  = "conf/ssl.key"
+
 	beego.Router("/custom", &controllers.CustomController{}, "get:ListTasks")
 	beego.Router("/wf0", &controllers.WF0Controller{}, "get:CreatePage")  			//angular
 	beego.Router("/react0", &controllers.React0Controller{}, "get:CreatePage")  //react
