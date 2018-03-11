027466057081897308dfa71cdf5e4d95922c459d
a good neighbor <shared@complicated.com>
2018-03-11 02:35:16 +0000

    https and redirect wor

--- main.go
+++ main.go
@@ -4,9 +4,15 @@ import (
 	_ "hellobeego/routers"
 	"hellobeego/controllers"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
+	"fmt"
 )
 
+var server_port int = 8089
+
 func main() {
+	beego.InsertFilter("/*", beego.BeforeStatic, RedirectHttp)  //catch all
+
 	beego.SetStaticPath("/css", "static/css")
 	beego.SetStaticPath("/vendor", "static/vendor")
 	beego.SetStaticPath("/app", "static/app")
@@ -14,16 +20,33 @@ func main() {
 	beego.SetStaticPath("/static", "static/reactsite/static")
 	beego.SetStaticPath("/", "static/reactsite")
 
-	beego.BConfig.Listen.EnableHTTP = false
+	beego.BConfig.Listen.EnableHTTP = true
 	beego.BConfig.Listen.EnableHTTPS = true
-	beego.BConfig.Listen.HTTPSPort  = 8080
-	beego.BConfig.Listen.HTTPSCertFile  = "conf/ssl.crt"
-	beego.BConfig.Listen.HTTPSKeyFile  = "conf/ssl.key"
+	beego.BConfig.Listen.HTTPSPort  = server_port
+	beego.BConfig.Listen.HTTPSCertFile  = "conf/ssl3.crt"
+	beego.BConfig.Listen.HTTPSKeyFile  = "conf/ssl3.key"
 
 	beego.Router("/custom", &controllers.CustomController{}, "get:ListTasks")
 	beego.Router("/wf0", &controllers.WF0Controller{}, "get:CreatePage")  			//angular
 	beego.Router("/react0", &controllers.React0Controller{}, "get:CreatePage")  //react
+	beego.Router("/simplewebrtc", &controllers.SimpleWebRTCController{}, "get:CreatePage")  //react
+
+	log("beego.Run()\n")
 	beego.Run()
 
 }
 
+var RedirectHttp = func(ctx *context.Context) {
+		log("RedirectHttp\n")
+    if !ctx.Input.IsSecure() {
+            // no need for an additional '/' between domain and uri
+        url := fmt.Sprintf( "https://%s:%d%s" , ctx.Input.Domain() ,
+									server_port , ctx.Input.URI() )
+				log(url)
+        ctx.Redirect(302, url)
+    }
+}
+
+func log(a string) {
+	fmt.Print(a)
+}
