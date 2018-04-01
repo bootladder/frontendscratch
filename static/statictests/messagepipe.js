b06d12d142c0828c8080663742d0287d1e575374
a good neighbor <shared@complicated.com>
2018-04-01 01:21:21 +0100

    move everything inside function scope

--- static/statictests/messagepipe.js
+++ static/statictests/messagepipe.js
@@ -74,163 +74,163 @@ function messagepipe_init(divContainer,mynameparam,yournameparam) {
     btnLoadB.onclick = function() {
         LoadPipe()
     }
-}
 
-function fetchAndUpdateMessageList(fetchParam, divParam) {
+    function fetchAndUpdateMessageList(fetchParam, divParam) {
 
-    app_ajax('query', function(res) {
-        updateMessageList(res,divParam)
-    },
-    fetchParam)
-}
-
-function updateMessageList(obj,mydiv) {
-
-    d = createMessageList(obj)
-    mydiv.innerHTML = ""
-    mydiv.appendChild(d) 
-}
-
-
-// Creates DOM element which can go inside a div tag
-// Takes an object with an array of message descriptors
-function createMessageList(obj) {
-    
-    d = document.createElement('div')
-    ul = document.createElement('ul')
-
-    console.log(obj)
-    parsedobj = JSON.parse(obj)
-    $.each(parsedobj, function(i, field){
-        var li = document.createElement('li');
-        m = createMessageListEntryFromMessageDesc(field)
-        li.appendChild(m)
-        ul.appendChild(li)
-    });
-    d.appendChild(ul)
-
-    //Populate Global Array of Message Descriptors
-    //Does not belong here!
-    $.each(parsedobj, function(i, field){
-        messageDescriptors[field.audioblobid] = field
-    });
-
-    return d
-}
-
-function createMessageListEntryFromMessageDesc(md) {
-
-    var d = document.createElement('div')
-    var playbutton = document.createElement('button')
-
-    // Set innerHTML of Button
-    playbutton.innerHTML = md.topic
-    if( md.customtopic ) {
-        playbutton.innerHTML = md.customtopic
+        app_ajax('query', function(res) {
+            updateMessageList(res,divParam)
+        },
+        fetchParam)
     }
 
-    // Set Color of Button
-    if( md.project == "1" ) {
-      color = "blue"
-    }
-    else if( md.project == "2" ) {
-      color = "orange"
-    }
-    else if( md.project == "3" ) {
-      color = "green"
-    }
-    else {
-      color = "gray"
+    function updateMessageList(obj,mydiv) {
+
+        d = createMessageList(obj)
+        mydiv.innerHTML = ""
+        mydiv.appendChild(d) 
     }
 
-    // Set Opacity of Button
-    if( md.listenedto == false )
-        opacity = "1"
-    else
-        opacity = "0.2"
 
-    playbutton.setAttribute("style", 
-      "background-color: "+color+";font-size : 32px; opacity:"+opacity); 
+    // Creates DOM element which can go inside a div tag
+    // Takes an object with an array of message descriptors
+    function createMessageList(obj) {
+        
+        d = document.createElement('div')
+        ul = document.createElement('ul')
 
-    playbutton.id= md.audioblobid
+        console.log(obj)
+        parsedobj = JSON.parse(obj)
+        $.each(parsedobj, function(i, field){
+            var li = document.createElement('li');
+            m = createMessageListEntryFromMessageDesc(field)
+            li.appendChild(m)
+            ul.appendChild(li)
+        });
+        d.appendChild(ul)
 
-    playbutton.onclick = listenToRecordedMessage
+        //Populate Global Array of Message Descriptors
+        //Does not belong here!
+        $.each(parsedobj, function(i, field){
+            messageDescriptors[field.audioblobid] = field
+        });
 
-
-    // Other Buttons
-    var replybutton  = document.createElement('button')
-    replybutton.innerHTML = "Reply"
-    replybutton.setAttribute("style", "background-color: green;");  
-    replybutton.onclick = replyRecordedMessage
-    replybutton.id = md.audioblobid
-
-    var vaultbutton  = document.createElement('button')
-    vaultbutton.innerHTML = "Vault"
-    vaultbutton.setAttribute("style", "background-color: orange;");  
-    vaultbutton.onclick = vaultRecordedMessage
-    vaultbutton.id = md.audioblobid
-
-    var erasebutton  = document.createElement('button')
-    erasebutton.innerHTML = "Del"
-    erasebutton.setAttribute("style", "background-color: red;");  
-    erasebutton.onclick = deleteRecordedMessage
-    erasebutton.id = md.audioblobid
-
-    d.appendChild(playbutton)
-    d.appendChild(replybutton)
-    d.appendChild(vaultbutton)
-    d.appendChild(erasebutton)
-    return d
-}
-////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////
-
-function replyRecordedMessage() {
-}
-function vaultRecordedMessage() {
-}
-
-////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////
-
-// Load the <audio> with the message
-// Also change the state to "listenedTo = true" and update
-function listenToRecordedMessage() {
-    console.log("loading3")
-    var audio = document.getElementById('audio-playback') || new Audio();
-    audio.src= "https://orbhub.bootladder.com:9002/audiomessagedownload/" + this.id;
-    audio.load()
-
-    if( messageDescriptors[this.id].listenedto == false ) {
-        console.log("Updating ListenedTo State to Server!")
-        updateMessageDescriptorListenedToState(this.id,true) 
-    }
-}
-////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////
-
-function deleteRecordedMessage() {
-
-    // Create Form with JSON filename
-    var s = {}
-    s.audioblobid = this.id
-
-    var successcb = function(myjson) { 
-        s = "Deleted Message." + myjson
-        alert( s )
+        return d
     }
 
-    app_ajax('delete', successcb, s)
-}
+    function createMessageListEntryFromMessageDesc(md) {
 
-////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////
+        var d = document.createElement('div')
+        var playbutton = document.createElement('button')
 
-function updateMessageDescriptorListenedToState(id,state) {
+        // Set innerHTML of Button
+        playbutton.innerHTML = md.topic
+        if( md.customtopic ) {
+            playbutton.innerHTML = md.customtopic
+        }
 
-    var s = {}
-    s.audioblobid = id
-    s.listenedto = state
-    
-    app_ajax('update', generic_ajax_success_callback , s)
+        // Set Color of Button
+        if( md.project == "1" ) {
+          color = "blue"
+        }
+        else if( md.project == "2" ) {
+          color = "orange"
+        }
+        else if( md.project == "3" ) {
+          color = "green"
+        }
+        else {
+          color = "gray"
+        }
+
+        // Set Opacity of Button
+        if( md.listenedto == false )
+            opacity = "1"
+        else
+            opacity = "0.2"
+
+        playbutton.setAttribute("style", 
+          "background-color: "+color+";font-size : 32px; opacity:"+opacity); 
+
+        playbutton.id= md.audioblobid
+
+        playbutton.onclick = listenToRecordedMessage
+
+
+        // Other Buttons
+        var replybutton  = document.createElement('button')
+        replybutton.innerHTML = "Reply"
+        replybutton.setAttribute("style", "background-color: green;");  
+        replybutton.onclick = replyRecordedMessage
+        replybutton.id = md.audioblobid
+
+        var vaultbutton  = document.createElement('button')
+        vaultbutton.innerHTML = "Vault"
+        vaultbutton.setAttribute("style", "background-color: orange;");  
+        vaultbutton.onclick = vaultRecordedMessage
+        vaultbutton.id = md.audioblobid
+
+        var erasebutton  = document.createElement('button')
+        erasebutton.innerHTML = "Del"
+        erasebutton.setAttribute("style", "background-color: red;");  
+        erasebutton.onclick = deleteRecordedMessage
+        erasebutton.id = md.audioblobid
+
+        d.appendChild(playbutton)
+        d.appendChild(replybutton)
+        d.appendChild(vaultbutton)
+        d.appendChild(erasebutton)
+        return d
+    }
+    ////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////
+
+    function replyRecordedMessage() {
+    }
+    function vaultRecordedMessage() {
+    }
+
+    ////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////
+
+    // Load the <audio> with the message
+    // Also change the state to "listenedTo = true" and update
+    function listenToRecordedMessage() {
+        console.log("loading3")
+        var audio = document.getElementById('audio-playback') || new Audio();
+        audio.src= "https://orbhub.bootladder.com:9002/audiomessagedownload/" + this.id;
+        audio.load()
+
+        if( messageDescriptors[this.id].listenedto == false ) {
+            console.log("Updating ListenedTo State to Server!")
+            updateMessageDescriptorListenedToState(this.id,true) 
+        }
+    }
+    ////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////
+
+    function deleteRecordedMessage() {
+
+        // Create Form with JSON filename
+        var s = {}
+        s.audioblobid = this.id
+
+        var successcb = function(myjson) { 
+            s = "Deleted Message." + myjson
+            alert( s )
+        }
+
+        app_ajax('delete', successcb, s)
+    }
+
+    ////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////
+
+    function updateMessageDescriptorListenedToState(id,state) {
+
+        var s = {}
+        s.audioblobid = id
+        s.listenedto = state
+        
+        app_ajax('update', generic_ajax_success_callback , s)
+    }
 }
