5e7cae35dd9053bcf8e9ca100435427bff94beaf
Steve Anderson <steve@bootladder.com>
2019-07-10 21:01:42 -0400

    delete button works, add center orb

--- static/statictests/elm/src/Main.elm
+++ static/statictests/elm/src/Main.elm
@@ -1,4 +1,4 @@
-port module Main exposing (MessageDescriptorResponseModel, MessageDescriptorViewModel, Model, Msg(..), attribute, filterMessagesFromUser, httpFetchMessages, httpJsonString, init, main, messageDescriptorResponseModelDecoder, messageOrbs, path, playbackMessage, queryDecoder, responseModel2ViewModel, selectedIndex, subscriptions, svgDestination, svgMessage, svgMessagePipe, svgPipe, svgSender, text, update, view, viewMessageMetadata)
+port module Main exposing (MessageDescriptorResponseModel, MessageDescriptorViewModel, Model, Msg(..), archiveButton, boundingRectangle, deleteButton, filterMessagesFromUser, httpDeleteMessage, httpFetchMessages, init, main, messageDescriptorResponseModelDecoder, messageOrbs, metadataButton, pixelRulerLength, playbackMessage, queryDecoder, replyButton, responseModel2ViewModel, selectedIndex, subscriptions, svgCenterOrb, svgDestination, svgMessagePipe, svgOrb, svgPipe, svgSender, text, update, updateJsonBody, updateMessageDescriptorListenedToState, view, viewMessageMetadata)
 
 import Basics.Extra exposing (..)
 import Browser
@@ -64,23 +64,39 @@ init a =
 -- HTTP Request  (Query for Books)
 
 
-httpJsonString : String -> String
-httpJsonString username =
-    Json.Encode.encode 0 <|
-        Json.Encode.object
-            [ ( "username", Json.Encode.string username )
-            ]
-
-
 httpFetchMessages : String -> Cmd Msg
 httpFetchMessages username =
     Http.post
         { body =
             Http.multipartBody
-                [ Http.stringPart "requestmodel" <| httpJsonString username
+                [ Http.stringPart "requestmodel" <|
+                    Json.Encode.encode 0 <|
+                        Json.Encode.object
+                            [ ( "username", Json.Encode.string username )
+                            ]
                 ]
         , url = "http://localhost:9002/audiomessageapi/queryusermessages"
         , expect = Http.expectJson ReceivedMessageDescriptorResponseModel queryDecoder
+        }
+
+
+
+-----------------
+
+
+httpDeleteMessage : MessageDescriptorViewModel -> Cmd Msg
+httpDeleteMessage messageDesc =
+    Http.post
+        { body =
+            Http.multipartBody
+                [ Http.stringPart "requestmodel" <|
+                    Json.Encode.encode 0 <|
+                        Json.Encode.object
+                            [ ( "audioblobid", Json.Encode.string messageDesc.id )
+                            ]
+                ]
+        , url = "http://localhost:9002/audiomessageapi/delete"
+        , expect = Http.expectWhatever ReceivedDeleteResponse
         }
 
 
@@ -135,6 +151,10 @@ type Msg
     | MessageOrbHovered MessageDescriptorViewModel
     | OrbClicked MessageDescriptorViewModel
     | UserSelectButtonClicked String
+    | ReplyButtonClicked MessageDescriptorViewModel
+    | ArchiveButtonClicked MessageDescriptorViewModel
+    | DeleteButtonClicked MessageDescriptorViewModel
+    | ReceivedDeleteResponse (Result Http.Error ())
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -153,6 +173,14 @@ update msg model =
             ( { model | hello = "FAIL" }
             , Cmd.none
             )
+
+        ReceivedDeleteResponse result ->
+            case result of
+                Ok _ ->
+                    ( model, httpFetchMessages model.user )
+
+                Err _ ->
+                    ( model, httpFetchMessages model.user )
 
         ReceivedMessageDescriptorResponseModel (Ok descriptors) ->
             let
@@ -184,6 +212,15 @@ update msg model =
 
         UserSelectButtonClicked user ->
             ( { model | user = user }, httpFetchMessages user )
+
+        ReplyButtonClicked messageDesc ->
+            ( { model | hello = "reply clicked" ++ messageDesc.sender }, Cmd.none )
+
+        DeleteButtonClicked messageDesc ->
+            ( { model | hello = "delete clicked" }, httpDeleteMessage messageDesc )
+
+        ArchiveButtonClicked messageDesc ->
+            ( { model | hello = "archive clicked" }, Cmd.none )
 
 
 responseModel2ViewModel : MessageDescriptorResponseModel -> MessageDescriptorViewModel
@@ -220,7 +257,8 @@ filterMessagesFromUser dest messages =
 view : Model -> Html Msg
 view model =
     div [ class "elm-svg" ]
-        [ div [ class "dice" ]
+        [ pixelRuler
+        , div [ class "dice" ]
             [ text model.hello
             ]
         , div [ class "elmuserlogin" ]
@@ -240,20 +278,17 @@ view model =
             ]
         , svg
             [ class "elmmessagepipes"
-            , width "500"
+            , width "500" --is actually 500px
             , height "500"
             , viewBox "0 0 1000 1000"
-            , fill "white"
-            , stroke "black"
-            , strokeWidth "3"
             ]
-            [ svgMessagePipe 0 model.user "aaron" <| filterMessagesFromUser "aaron" model.messageDescriptors
+            [ boundingRectangle
+            , svgMessagePipe 0 model.user "aaron" <| filterMessagesFromUser "aaron" model.messageDescriptors
             , svgMessagePipe 30 model.user "user1" <| filterMessagesFromUser "user1" model.messageDescriptors
             , svgMessagePipe 60 model.user "user2" <| filterMessagesFromUser "user2" model.messageDescriptors
             , svgMessagePipe 90 model.user "steve" <| filterMessagesFromUser "steve" model.messageDescriptors
-            ]
-        , div [ class "elmmessagemetadata" ]
-            [ viewMessageMetadata model.selectedMessage
+            , svgCenterOrb model.user
+            , viewMessageMetadata model.selectedMessage
             ]
         ]
 
@@ -261,7 +296,7 @@ view model =
 svgMessagePipe : Int -> String -> String -> List MessageDescriptorViewModel -> Html Msg
 svgMessagePipe angle myname yourname messageDescriptors =
     svg
-        [ width "500"
+        [ width "500" -- is actually 250px due to the parent 1000 viewBox
         , height "80"
         , viewBox "0 0 1000 100"
         , fill "white"
@@ -300,27 +335,28 @@ messageOrbs messageDescriptors =
         desc_offset_tuples =
             List.map2 Tuple.pair x_offsets messageDescriptors
     in
-    List.map (uncurry svgMessage) desc_offset_tuples
+    List.map (uncurry svgOrb) desc_offset_tuples
 
 
-svgMessage x_offset messageDesc =
+svgOrb x_offset messageDesc =
     svg
         [ width "10%"
         , height "100%"
-        , viewBox "0 0 300 300"
+
+        --     , viewBox "0 0 300 300"
         , fill messageDesc.backgroundColor
         , stroke "black"
         , strokeWidth "3"
         , x x_offset
         , y "0"
+        , Svg.Events.onClick <| OrbClicked messageDesc
+        , Svg.Events.onMouseOver <| MessageOrbHovered messageDesc
+        , Svg.Events.onMouseOut <| Hello "svg OUT"
         ]
         [ rect [ x "0", y "0", width "100%", height "100%", rx "1", ry "1" ] []
         , circle
             [ cx "50%"
             , cy "50%"
-            , Svg.Events.onClick <| OrbClicked messageDesc
-            , Svg.Events.onMouseOver <| MessageOrbHovered messageDesc
-            , Svg.Events.onMouseOut <| Hello "svg OUT"
             , r "30%"
             , fill messageDesc.color
             ]
@@ -328,15 +364,32 @@ svgMessage x_offset messageDesc =
         , text_
             [ x "40%"
             , y "60%"
-            , fontSize "90"
-            , Svg.Events.onClick <| OrbClicked messageDesc
+            , fontSize "2em"
+
+            --, Svg.Events.onClick <| OrbClicked messageDesc
             ]
             [ text messageDesc.label ]
         ]
 
 
 svgPipe =
-    rect [ x "1", y "1", width "1000", height "100", rx "15", ry "15" ] []
+    rect [ class "svgPipe", width "100%", height "100%" ] []
+
+
+svgCenterOrb name =
+    svg
+        []
+        [ circle
+            [ cx "0"
+            , cy "0"
+            , Svg.Events.onClick <| Hello "svg clcked"
+            , Svg.Events.onMouseOver <| Hello "svg OVER"
+            , Svg.Events.onMouseOut <| Hello "svg OUT"
+            , r "15%"
+            , fill "brown"
+            ]
+            []
+        ]
 
 
 svgSender name =
@@ -368,7 +421,7 @@ svgSender name =
 svgDestination name =
     svg
         [ width "20%"
-        , height "100%"
+        , height "200%"
         , viewBox "0 0 300 300"
         , fill "gray"
         , stroke "black"
@@ -388,6 +441,7 @@ svgDestination name =
             ]
             []
         , text_ [ x "40%", y "60%", fontSize "90" ] [ text name ]
+        , boundingRectangle
         ]
 
 
@@ -395,28 +449,124 @@ viewMessageMetadata : Maybe MessageDescriptorViewModel -> Html Msg
 viewMessageMetadata maybemessage =
     case maybemessage of
         Just message ->
-            div []
-                [ text message.sender
-                , text message.destination
-                , text message.color
-                , text message.label
-                , text "REPLY WITH SAME PARAMS BUTTON"
+            svg []
+                [ svg
+                    [ x "0"
+                    , y "75%"
+                    , height "25%"
+                    ]
+                    [ boundingRectangle
+                    , svg
+                        [ x "0%"
+                        , y "0%"
+                        , width "30%"
+                        ]
+                        [ replyButton message ]
+                    , svg
+                        [ x "33%"
+                        , y "0%"
+                        , width "30%"
+                        ]
+                        [ deleteButton message ]
+                    , svg
+                        [ x "66%"
+                        , y "0%"
+                        , width "30%"
+                        ]
+                        [ archiveButton message ]
+                    , text_
+                        [ x "0"
+                        , y "75%"
+                        , fontSize "2em"
+                        ]
+                        [ text "HURR" ]
+                    ]
                 ]
 
         Nothing ->
-            div [] [ text "nothing" ]
+            svg
+                [ x "0"
+                , y "75%"
+                , height "25%"
+                ]
+                [ boundingRectangle
+                ]
 
 
-attribute =
-    Html.Attributes.attribute
+replyButton message =
+    metadataButton "REPLY" message ReplyButtonClicked
+
+
+deleteButton message =
+    metadataButton "DELETE" message DeleteButtonClicked
+
+
+archiveButton message =
+    metadataButton "ARCHIVE" message ArchiveButtonClicked
+
+
+metadataButton textParam message cmd =
+    svg
+        [ height "100%"
+        , viewBox "0 0 300 300"
+        , fill "white"
+        , class "metadatabutton"
+        , Svg.Events.onClick <| cmd message
+
+        --, stroke "black"
+        , strokeWidth "3"
+        , x "0%"
+        , y "0"
+        ]
+        [ rect [ x "0", y "0", width "100%", height "100%", rx "1", ry "1" ] []
+        , circle
+            [ cx "50%"
+            , cy "50%"
+            , r "30%"
+            , fill "brown"
+            ]
+            []
+        , text_ [ x "30%", y "50%", fontSize "2em" ] [ text textParam ]
+        ]
+
+
+boundingRectangle =
+    svg
+        [ fill "white"
+        , strokeWidth "5"
+        , stroke "black"
+        ]
+        [ rect [ x "0", y "0", width "100%", height "100%", rx "1", ry "1" ] [] ]
+
+
+
+-- DEBUG HELPER CRAP
+
+
+pixelRulerLength len =
+    div
+        [ Html.Attributes.style "background-color" "red"
+        , Html.Attributes.style "height" "2px"
+        , Html.Attributes.style "width" len
+        ]
+        []
+
+
+pixelRuler =
+    div [ class "pixelruler" ]
+        [ pixelRulerLength "10px"
+        , pixelRulerLength "20px"
+        , pixelRulerLength "30px"
+        , pixelRulerLength "40px"
+        , pixelRulerLength "50px"
+        , pixelRulerLength "100px"
+        , pixelRulerLength "200px"
+        , pixelRulerLength "500px"
+        ]
 
 
 text =
     Svg.text
-
-
-path =
-    Svg.path
 
 
 port selectedIndex : (Value -> msg) -> Sub msg
